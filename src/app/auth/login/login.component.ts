ec5ba5509bbaaf63f9925c3babe8d1dd590a87bc
Vladyslav <vlad.drobot32@gmail.com>
2022-07-16 14:14:12 +0300

    log-in section changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserAuthService } from 'src/app/services/user-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  logInFormGroup: FormGroup;
+
+  constructor(private  authService: UserAuthService) { }
 
   ngOnInit(): void {
+    this.logInFormGroup = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null)
+    })
+    
+  }
+  
+  onSubmit() {
+    if(this.logInFormGroup.valid) {
+      this.authService.getUsers(this.logInFormGroup.value.password).subscribe(data => {
+        console.log(data);
+      })
+      console.log(this.logInFormGroup.value);
+      return;
+    }
+    console.error('Invalid form')
   }
 
 }
