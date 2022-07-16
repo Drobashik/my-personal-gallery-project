42e10ce9a9f554d538e0eefd3e7f8fabad1506ee
Vladyslav <vlad.drobot32@gmail.com>
2022-07-16 14:13:59 +0300

    sign-up section

--- src/app/auth/sign-up/signup.component.ts
+++ src/app/auth/sign-up/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserAuthService } from 'src/app/services/user-auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpFormGroup: FormGroup;
+  areValidPasswords: boolean = true;
+
+  constructor(private userAuthService: UserAuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.signUpFormGroup = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(null, [Validators.required,  Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      repeatedPassword: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    })
+  }
+
+  onSubmitSignUpForm(): void {
+    this.areValidPasswords = this.userAuthService.checkPasswordsValidation(this.signUpFormGroup)
+    if (this.signUpFormGroup.valid && this.areValidPasswords) {
+      console.log(this.signUpFormGroup.value);
+      const user: User = {
+        name: this.signUpFormGroup.value.name,
+        email: this.signUpFormGroup.value.email,
+        password: this.signUpFormGroup.value.password,
+      }
+      this.userAuthService.createUser(user).subscribe((data) => {
+        this.router.navigate(['/log-in'])
+      })
+      return;
+    }
+    this.signUpFormGroup.invalid
   }
 
 }
